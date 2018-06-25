3a5431ff6af455b915b1a117a4275773b923fc40
flux666 <38372883+flux666@users.noreply.github.com>
2018-06-25 08:50:06 +0200

    Initial commit

---
+++ hub/src/fr/flux666/hub/main.java
@@ -0,0 +1,16 @@
+package fr.flux666.hub;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin implements Listener {
+
+	public void onEnable() {
+		System.out.print("hub Enable");
+	}
+	
+	public void onDisable() {
+		System.out.print("hub Disable");
+	}
+	
+}
